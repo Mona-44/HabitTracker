ee2d4f1d2a868ced7477ee1cefccfbe71a2f66c4
Joseph Omale <mrjay0505@gmail.com>
2026-09-21 11:17:37 +0100

    Signup Handling with React-Hook-Form & Zod

--- src/screens/auth/signup.tsx
+++ src/screens/auth/signup.tsx
@@ -5,6 +5,7 @@ import {
   Image,
   TextInput,
   TouchableOpacity,
+  Alert,
 } from "react-native";
 import {
   widthPercentageToDP as wp,
@@ -12,8 +13,52 @@ import {
 } from "react-native-responsive-screen";
 import { Ionicons } from "@expo/vector-icons";
 import { COLORS } from "../../utils/colors";
+import { useState, useEffect } from "react";
+import { z } from "zod";
+import { useForm, Controller } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 export default function Signup() {
+  const onSubmit = (data: SignupType) => {
+    console.log(data);
+  };
+
+  const signupSchema = z
+    .object({
+      fullName: z.string().min(4, "Full name must be at least 4 characters."),
+      email: z.email("Please enter a valid email"),
+      password: z
+        .string()
+        .min(8, "Password must be at least 8 characters")
+        .regex(/[A-Z]/, "Password must have at least one uppercase letter")
+        .regex(/[a-z]/, "Password must have at least one lowercase letter")
+        .regex(/[0-9]/, "Password must have at least one number")
+        .regex(
+          /[^a-zA-Z0-9]/,
+          "Password must have at least one special character",
+        ),
+      passwordConfirmation: z.string().min(8, "Both passwords must be equal"),
+    })
+    .refine((data) => data.password === data.passwordConfirmation, {
+      message: "Password must match",
+      path: ["passwordConfirmation"],
+    });
+  type SignupType = z.infer<typeof signupSchema>;
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors, touchedFields },
+  } = useForm<SignupType>({
+    defaultValues: {
+      fullName: "",
+      email: "",
+      password: "",
+      passwordConfirmation: "",
+    },
+    resolver: zodResolver(signupSchema),
+  });
+
   return (
     <View style={styles.container}>
       {/* Logo Section */}
@@ -28,6 +73,7 @@ export default function Signup() {
       <View style={styles.header}>
         <Text style={styles.title}>Create Account</Text>
         <Text style={styles.subtitle}>Start your journey with HabitTrack</Text>
+        {/* <Text style={styles.subtitle}>Welcome {name}</Text> */}
       </View>
 
       {/* Form Section */}
@@ -45,12 +91,24 @@ export default function Signup() {
               color="gray"
             />
             <View style={styles.inputView}>
-              <TextInput
-                style={styles.input}
-                placeholder="Enter your full name"
+              <Controller
+                control={control}
+                name="fullName"
+                render={({ field: { onChange, value, onBlur } }) => (
+                  <TextInput
+                    style={styles.input}
+                    placeholder="Enter your full name"
+                    value={value}
+                    onChangeText={onChange}
+                    onBlur={onBlur}
+                  />
+                )}
               />
             </View>
           </View>
+          {touchedFields.fullName && errors.fullName && (
+            <Text style={styles.errorText}>{errors.fullName?.message}</Text>
+          )}
         </View>
 
         {/* Email Input */}
@@ -66,9 +124,26 @@ export default function Signup() {
               color="gray"
             />
             <View style={styles.inputView}>
-              <TextInput style={styles.input} placeholder="Enter your email" />
+              <Controller
+                control={control}
+                name="email"
+                render={({ field: { value, onChange, onBlur } }) => (
+                  <TextInput
+                    style={styles.input}
+                    placeholder="Enter your email"
+                    keyboardType="email-address"
+                    value={value}
+                    onChangeText={onChange}
+                    onBlur={onBlur}
+                  />
+                )}
+              />
             </View>
           </View>
+
+          {touchedFields.email && errors.email && (
+            <Text style={styles.errorText}>{errors.email?.message}</Text>
+          )}
         </View>
 
         {/* Password Input */}
@@ -84,13 +159,26 @@ export default function Signup() {
               color="gray"
             />
             <View style={styles.inputView}>
-              <TextInput
-                style={styles.input}
-                placeholder="Enter your password"
-                secureTextEntry={true} // Changed
+              <Controller
+                control={control}
+                name="password"
+                render={({ field: { value, onChange, onBlur } }) => (
+                  <TextInput
+                    style={styles.input}
+                    placeholder="Enter your pasword"
+                    secureTextEntry={true}
+                    value={value}
+                    onChangeText={onChange}
+                    onBlur={onBlur}
+                  />
+                )}
               />
             </View>
           </View>
+
+          {touchedFields.password && errors.password && (
+            <Text style={styles.errorText}>{errors.password?.message}</Text>
+          )}
         </View>
 
         {/* Confirm Password Input */}
@@ -106,22 +194,38 @@ export default function Signup() {
               color="gray"
             />
             <View style={styles.inputView}>
-              <TextInput
-                style={styles.input}
-                placeholder="Confirm your password"
-                secureTextEntry={true} // Changed
+              <Controller
+                control={control}
+                name="passwordConfirmation"
+                render={({ field: { value, onChange, onBlur } }) => (
+                  <TextInput
+                    style={styles.input}
+                    placeholder="Confirm your password"
+                    secureTextEntry={true}
+                    value={value}
+                    onChangeText={onChange}
+                    onBlur={onBlur}
+                  />
+                )}
               />
             </View>
           </View>
+
+          {touchedFields.passwordConfirmation &&
+            errors.passwordConfirmation && (
+              <Text style={styles.errorText}>
+                {errors.passwordConfirmation?.message}
+              </Text>
+            )}
         </View>
 
         {/* Button */}
-        <TouchableOpacity style={styles.btn}>
+        <TouchableOpacity style={styles.btn} onPress={handleSubmit(onSubmit)}>
           <Text style={styles.btnText}>Sign Up</Text>
         </TouchableOpacity>
       </View>
       <View style={styles.bottom}>
-        <Text style={styles.bottomText}>Already have an accound?</Text>
+        <Text style={styles.bottomText}>Already have an account?</Text>
         <Text style={styles.bottomLink}>Log in</Text>
       </View>
     </View>
@@ -144,11 +248,11 @@ const styles = StyleSheet.create({
   },
   title: {
     fontFamily: "Bold",
-    fontSize: 36,
+    fontSize: 32, // changed
   },
   subtitle: {
     fontFamily: "Regular",
-    fontSize: 20,
+    fontSize: 18, // changed
     color: "gray",
   },
   form: {
@@ -162,8 +266,8 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     gap: wp("2%"),
     alignItems: "center",
-    paddingHorizontal: 16, // Changed
-    paddingVertical: 8, // Changed
+    paddingHorizontal: wp("3%"), // Changed again
+    minHeight: hp("6.5%"), // Changed again
   },
   inputView: {
     flexGrow: 1,
@@ -174,14 +278,14 @@ const styles = StyleSheet.create({
   },
   label: {
     fontFamily: "Bold",
-    fontSize: 20,
+    fontSize: 18,
     marginBottom: hp("0.8%"),
   },
   btn: {
     backgroundColor: COLORS.bgColor, // Changed
     justifyContent: "center", // Changed
     alignItems: "center", // Changed
-    padding: wp("3%"), // Changed
+    paddingVertical: wp("3%"), // Changed again
     borderRadius: 8, // Changed
     marginTop: hp("2%"), // Changed
   },
@@ -193,15 +297,19 @@ const styles = StyleSheet.create({
   },
   bottom: {
     flexDirection: "row", // Changed
-    gap: 8, // Changed
+    gap: 8, // Changed again
   },
   bottomText: {
     fontFamily: "Medium", // Changed
-    fontSize: 20, // Changed
+    fontSize: 16, // Changed again
   },
   bottomLink: {
     fontFamily: "Medium", // Changed
-    fontSize: 20, // Changed
+    fontSize: 16, // Changed
     color: COLORS.bgColor, // Changed
+  },
+  errorText: {
+    color: "red",
+    fontSize: 16,
   },
 });
